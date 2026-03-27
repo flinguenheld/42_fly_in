720d9c90aa81c987c0a89f51a7275eaa834f4b55
flinguen <florent@linguenheld.net>
2026-03-27 13:10:44 +0100

    Add Point & up gitignore

---
+++ point.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from math import sqrt
+from typing import Tuple
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀█░█▀█░▀█▀░█▀█░▀█▀
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀▀░█░█░░█░░█░█░░█░
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░
+class Point:
+    def __init__(
+        self,
+        row: int = 0,
+        col: int = 0,
+        x: int | None = None,
+        y: int | None = None,
+    ) -> None:
+        """2D point, saved with row/col values"""
+        if x and y:
+            self.row: int = y
+            self.col: int = x
+        else:
+            self.row: int = row
+            self.col: int = col
+
+    def __str__(self):
+        return f"({self.row})/{self.col})"
+
+    # ########################################################################
+    # ############################################################# X / Y ####
+    def to_xy(self) -> Tuple[int, int]:
+        return (self.col, self.row)
+
+    # ########################################################################
+    # ###################################################### CALCULATIONS ####
+    def width(self, to: Point) -> int:
+        return abs((to.col - self.col))
+
+    def height(self, to: Point) -> int:
+        return abs((to.row - self.row))
+
+    def distance(self, to: Point) -> int:
+        return int(sqrt(self.width(to) ** 2 + self.height(to) ** 2))
